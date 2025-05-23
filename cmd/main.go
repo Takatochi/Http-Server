125b5a6ac687d0290c1556fc81f411a2343bebf2
Takatochi <nazera55tigr66t28@gmail.com>
2025-05-23 19:01:36 +0300

    create

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,14 @@
 package main
+
+import (
+	"fmt"
+	"htt-server/internal/app"
+)
+
+func main() {
+
+	err := app.Run()
+	if err != nil {
+		fmt.Println(err)
+	}
+}
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,38 @@
 package server
+
+import (
+	"context"
+	"net/http"
+)
+
+type Http struct {
+	server *http.Server
+}
+
+func NewHttp(addr string) *Http {
+	mux := http.NewServeMux() // router
+
+	handler := NewHandler()
+	// https://domen/ https://rozetka.com.ua/ -> index
+	mux.HandleFunc("/", HelloHandler) // endpoint
+	// https://domen/application/hello https://rozetka.com.ua/application/hello -> index
+	// http://localhost:8080/application/hello?name=Arthur&typeOf=3
+	mux.HandleFunc("/application/hello", handler.HelloAPPHandler) // endpoint
+
+	server := &http.Server{
+		Addr:    addr,
+		Handler: mux,
+	}
+
+	return &Http{
+		server: server,
+	}
+}
+
+func (h *Http) Start() error {
+	return h.server.ListenAndServe()
+}
+
+func (h *Http) Stop(ctx context.Context) error {
+	return h.server.Shutdown(ctx)
+}
